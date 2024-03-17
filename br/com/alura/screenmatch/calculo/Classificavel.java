580f5e726c255588237ccde42bd8c62af2c57d07
Ariádine Vieira Amaral <ariadinevamaral@gmail.com>
2024-03-16 21:35:53 -0300

    Update Classificavel.java

--- br/com/alura/screenmatch/calculo/Classificavel.java
+++ br/com/alura/screenmatch/calculo/Classificavel.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch.calculo;
 
 public interface Classificavel {
-
-	//Interface é para herdar mais de uma classe, 
-	//pois o exteds só permite uma herança
 	
 	int getClassificacao();
 	
